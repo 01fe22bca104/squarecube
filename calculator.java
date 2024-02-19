b73c1115a6fa0e138d24621535eb65c75cbfafcd
Saadhvi <01fe22bca104@kletech.ac.in>
2024-02-19 14:44:37 +0530

    Program of extended class

---
+++ calculator.java
@@ -0,0 +1,27 @@
+public class calculator 
+{
+   
+
+    void addition(int a,int b){
+        int sum=a+b;
+        System.out.println("Substraction of the number is:"+sum);
+
+    }
+    void substraction(int a, int b){
+        int diff=a-b;
+        System.out.println("Substraction of the number is:"+diff);
+        
+    }
+    void multiplication(int a,int b){
+        int prod=a*b;
+        System.out.println("Multiplication of the number is:"+prod);
+        
+    }
+    void divison(int a,int b){
+        int div=a/b;
+        System.out.println("Division of the number is:"+div);
+        
+    }
+
+   
+}
